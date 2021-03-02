46b1f78e0343530092f31d77b88aab8cc1108d7b
florin-ciornei <31960645+florin-ciornei@users.noreply.github.com>
2021-03-02 11:23:02 +0100

    Create & update doc for user registration

---
+++ doc/api_project.js
@@ -0,0 +1,18 @@
+define({
+  "name": "Time Capsule BPR Project",
+  "version": "0.0.1",
+  "description": "This is our API documentation for our Bachelor project",
+  "title": "BPR API documentation",
+  "template": {
+    "forceLanguage": "en"
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-03-02T10:21:49.180Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
