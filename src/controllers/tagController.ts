d25c4cc42b84409e79b0f1adcb175308be773584
florin-ciornei <31960645+florin-ciornei@users.noreply.github.com>
2021-03-24 10:55:12 +0100

    Implement get tag suggestions and get all tags routes

--- src/controllers/tagController.ts
+++ src/controllers/tagController.ts
@@ -6,10 +6,42 @@ import TimeCapsuleModel, { TimeCapsule } from '../schemas/timeCapsuleSchema';
 const router = express.Router();
 
 /**
- * Get tag suggestions
+ * Get all tags.
+ */
+router.get("/all", async (req, res) => {
+	let tags = await TimeCapsuleModel.aggregate([
+		{ $unwind: "$tags" },
+		{ $project: { tags: 1 } },
+		{ $group: { _id: "$tags" } }
+	]);
+	//after the above aggregate we get an array of json objects [{_id:'tag name'}]. The following line transforms it into an array of strings (tags).
+	tags = tags.map(t => t._id);
+	res.json({
+		status: "success",
+		results: tags.length,
+		tags: tags,
+	});
+});
+
+
+/**
+ * Get tag suggestions.
  */
 router.get("/suggestions/:query", async (req, res) => {
-
+	let query = req.params.query;
+	let tags = await TimeCapsuleModel.aggregate([
+		{ $unwind: "$tags" },
+		{ $project: { tags: 1 } },
+		{ $group: { _id: "$tags" } },
+		{ $match: { _id: { '$regex': query, '$options': 'i' } } }
+	]).limit(20);
+	//after the above aggregate we get an array of json objects [{_id:'tag name'}]. The following line transforms it into an array of strings (tags).
+	tags = tags.map(t => t._id);
+	res.json({
+		status: "success",
+		results: tags.length,
+		tags: tags,
+	});
 });
 
 export default router;
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import * as mongoose from 'mongoose';
 import UserController from './controllers/userController';
 import GroupController from './controllers/groupController';
 import TimeCapsuleController from './controllers/timeCapsuleController';
+import TagController from './controllers/tagController';
 
 //router imports
 import { devAuthRouter } from './routers/authRouters';
@@ -24,6 +25,7 @@ app.use(devAuthRouter); //authorization header
 //add controllers to express app
 app.use('/user', UserController);
 app.use('/group', GroupController);
+app.use('/tag', TagController);
 app.use('/timeCapsule', TimeCapsuleController);
 
 //connect to MongoDB and start the server
