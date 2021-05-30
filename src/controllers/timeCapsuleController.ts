1cc4a2b6312ef451c25d1ba9c2b3fee6e9d13cdf
florin-ciornei <31960645+florin-ciornei@users.noreply.github.com>
2021-05-30 16:58:02 +0200

    Update timeCapsuleController.ts

--- src/controllers/timeCapsuleController.ts
+++ src/controllers/timeCapsuleController.ts
@@ -46,13 +46,13 @@ router.post('/', requireAuth, upload.array('contents'), async (req, res) => {
 	let backgroundType: number = req.body.backgroundType;
 
 	// while testing allow creating capsules with a past date
-	if (process.env.NODE_ENV != 'test' && process.env.NODE_ENV != 'dev' && openDate < new Date()) {
-		return res.status(400).send({
-			status: 'error',
-			code: 'invalid_open_data',
-			message: 'Open date must be in the future!'
-		});
-	}
+	// if (process.env.NODE_ENV != 'test' && process.env.NODE_ENV != 'dev' && openDate < new Date()) {
+	// 	return res.status(400).send({
+	// 		status: 'error',
+	// 		code: 'invalid_open_data',
+	// 		message: 'Open date must be in the future!'
+	// 	});
+	// }
 
 	if (name.length < 3 || name.length > 32) {
 		return res.status(400).send({
